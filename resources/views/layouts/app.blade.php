d666d08df5a125ef817aa6d08184902b7c27960c
Jessica Dionisio <dionisiojessica15@gmail.com>
2018-08-21 14:49:02 +0800

    Todo Show Page and Includes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>TodoList</title>
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
   </head>
   <body>
+    @include('inc.navbar')
     <div class="container">
+      @include('inc.messages')
       @yield('content')
     </div>
 
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -5,8 +5,8 @@
   @if(count($todos)>0)
     @foreach($todos as $todo)
     <div class="well">
-      <h3 class="well">{{$todo->text}}</h3>
-      <span class="label label-danger">{{$todo->due}}</span>
+      <h3 class="well"><a href="todo/{{$todo->id}}"> {{$todo->text}} </a><span class="label label-danger">{{$todo->due}}</span></h3>
+
     </div>
     @endforeach
   @endif
