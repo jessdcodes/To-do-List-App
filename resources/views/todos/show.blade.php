3da87a88e14222448f8d2664ce75ba0616ff3911
Jessica Dionisio <dionisiojessica15@gmail.com>
2018-08-28 09:23:56 +0800

    Edit and Update Todos

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-  <a class="btn btn-default" href="/">Go Back</a>
+  <a class="btn btn-default" href="/todolist/public">Go Back</a>
   <h1 class="well">{{$todo->text}}</h1>
   <div class="label label-danger">{{$todo->due}}</div>
   <hr>
   <p>{{$todo->body}}</p>
+  <br>
+  <a href="{{$todo->id}}/edit" class ="btn btn-default">Edit</a>
 @endsection
