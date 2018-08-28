c2d7594efb340106cfacaf9cfe700c4f287b9b32
Jessica Dionisio <dionisiojessica15@gmail.com>
2018-08-28 09:55:40 +0800

    Delete Todos

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -8,4 +8,8 @@
   <p>{{$todo->body}}</p>
   <br>
   <a href="{{$todo->id}}/edit" class ="btn btn-default">Edit</a>
+  {!! Form::open(['action' => ['TodosController@destroy', $todo->id], 'class' => 'pull-right']) !!}
+    {{ Form::hidden('_method', 'DELETE')}}
+    {{ Form::bsSubmit('Delete', ['class' => 'btn btn-danger']) }}
+  {!! Form::close() !!}
 @endsection
